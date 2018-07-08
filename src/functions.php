7a1b00a7e00e02a4652f4804edd3463016cd5744
Inhere <inhere@users.noreply.github.com>
2018-07-08 12:04:58 +0800

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -19,3 +19,35 @@ if (!function_exists('co')) {
 //         return \SwooleKit\Util\Coroutine::await($cb);
 //     }
 // }
+
+function await(Closure $fn) {
+    $ch = new chan(1);
+
+    go(function () use($fn, $ch) {
+        $ret = $fn();
+        $ch->push($ret);
+    });
+
+    // var_dump(\Co::getuid());
+    return $ch->pop();
+}
+
+function await_multi(Closure ...$fns) {
+    $len = count($fns);
+    $ch = new chan($len);
+
+    foreach ($fns as $fn) {
+        go(function () use($fn, $ch) {
+            $ret = $fn();
+            $ch->push($ret);
+        });
+    }
+
+    $results = [];
+
+    for ($i = 0; $i < $len; $i++) {
+        $results[] = $ch->pop();
+    }
+
+    return $results;
+}
